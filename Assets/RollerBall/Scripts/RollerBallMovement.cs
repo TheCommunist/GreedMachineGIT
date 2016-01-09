7f4a516d0142d8bf62ee106099b0bce38f53f6f9
B Chancellor <b.k.chancellor@gmail.com>
2016-01-09 17:16:48 -0600

    fixed ignore and added scripts

---
+++ Assets/RollerBall/Scripts/RollerBallMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollerBallMovement : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float speedModifier = 1.0f;
+
+	void Start()
+	{
+		rb = GetComponentInChildren<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHoriz = Input.GetAxis("Horizontal");
+		float moveVert = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHoriz , 0.0f, moveVert);
+
+		rb.AddForce(movement * speedModifier);
+	}
+}
