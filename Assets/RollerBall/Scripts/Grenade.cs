709fb5497f7ffcaaf32e0ebe730abc0389dc2942
B Chancellor <b.k.chancellor@gmail.com>
2016-01-10 13:41:08 -0600

    Added grenade code for testing

---
+++ Assets/RollerBall/Scripts/Grenade.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grenade : MonoBehaviour 
+{
+	public float delayTime = 1.0f;
+	public float explosionForce;
+	public float explosionRadius;
+
+	void Start()
+	{
+		StartCoroutine(Explode());
+	}
+
+	IEnumerator Explode()
+	{
+		yield return new WaitForSeconds(delayTime);
+
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+
+		foreach(Collider hit in colliders)
+		{
+			Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+			if (rb != null)
+			{
+				rb.AddExplosionForce(explosionForce, explosionPos, explosionRadius, 100.0f);
+			}
+		}
+
+		Destroy(gameObject);
+	}
+}
--- Assets/RollerBall/Scripts/TestFire.cs
+++ Assets/RollerBall/Scripts/TestFire.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestFire : MonoBehaviour 
 {
@@ -7,6 +8,11 @@ public class TestFire : MonoBehaviour
 	public GameObject destination;
 	public Rigidbody grenadePrefab;
 	public float grenadeSpeed;
+
+	public int maxNumberOfFieldedGrenades= 1;
+	List<GameObject> activeGrenades = new List<GameObject>();
+
+	private Vector3 fireOrigin;
 
 	private Vector3 aimVector;
 
@@ -21,6 +27,8 @@ public class TestFire : MonoBehaviour
 		aimVector = destination.transform.position - source.transform.position;
 		Debug.DrawRay(source.transform.position, aimVector, Color.white);
 
+		fireOrigin = new Vector3(source.transform.position.x + (Mathf.Clamp(aimVector.x, -1, 1)), source.transform.position.y, source.transform.position.z + (Mathf.Clamp(aimVector.z, -1 , 1)));
+
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
 			FireGrenade();
@@ -29,13 +37,29 @@ public class TestFire : MonoBehaviour
 
 	void FireGrenade()
 	{
-		Rigidbody grenadeClone; 
-		grenadeClone = Instantiate(grenadePrefab, destination.transform.position,transform.rotation) as Rigidbody;
+		//check to see how many active grenades have been fired
+		for(int i = 0; i < activeGrenades.Count; i++)
+		{
+			print(activeGrenades[i].gameObject);
+			if(activeGrenades[i].gameObject == null)
+				activeGrenades.RemoveAt(i);
+		}
 
-		if(grenadeClone != null)
-			print("its here");
-		else
-			print("Lost it dude");
-		//grenadeClone.AddForce(aimVector
+		print(activeGrenades.Count);
+		if(activeGrenades.Count < maxNumberOfFieldedGrenades)
+		{
+			Rigidbody grenadeClone; 
+
+			grenadeClone = Instantiate(grenadePrefab, fireOrigin, transform.rotation) as Rigidbody;
+
+			if(grenadeClone != null)
+			{
+				grenadeClone.AddForce(aimVector *200);
+				activeGrenades.Add( grenadeClone.gameObject);
+			}
+			else
+				print("Lost grenade prefab reference");
+		}
+
 	}
 }
